3840774b7600a1e884e1aa18cb1d2bdf10d6f06e
HarshulDang <dangharshul199430@gmail.com>
2018-02-14 12:47:02 +0530

    first message

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var Product = require('../models/product');
+var csrf = require('csurf');
+
+var csrfProtection = csrf();
+router.use(csrfProtection);
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	Product.find(function(err,docs){
+		var productChunks = [];
+		var chunksize = 3;
+		for(var i = 0; i<docs.length;i+=chunksize){
+			productChunks.push(docs.slice(i,i+chunksize));
+		}
+		res.render('shop/index', { title: 'Shopping Cart',products: productChunks });	
+	});
+  
+});
+
+router.get('/user/signup',function(req,res,next){
+	res.render('user/signup',{csrfToken:req.csrfToken()});
+});
+
+module.exports = router;
---
+++ seed/product-seeder.js
@@ -0,0 +1,64 @@
+var Product = require('../models/product');
+
+var mongoose = require('mongoose');
+mongoose.connect('localhost:27017/shopping');
+
+
+var products = [
+	new Product({
+		imagePath: 'https://upload.wikimedia.org/wikipedia/en/5/5e/Gothiccover.png',
+		title:   'Gothic Video Game',
+		description: 'Awesome Game!!!!',
+		price: 10
+	}),
+
+	new Product({
+		imagePath: 'https://upload.wikimedia.org/wikipedia/en/5/5e/Gothiccover.png',
+		title:   'Gothic Video Game',
+		description: 'Awesome',
+		price: 20
+	}),
+
+	new Product({
+		imagePath: 'https://upload.wikimedia.org/wikipedia/en/5/5e/Gothiccover.png',
+		title:   'Gothic Video Game',
+		description: 'Now that is super Awesome Game!!!!',
+		price: 30
+	}),
+
+	new Product({
+		imagePath: 'https://upload.wikimedia.org/wikipedia/en/5/5e/Gothiccover.png',
+		title:   'Gothic Video Game',
+		description: 'Ohh Fuck',
+		price: 40
+	}),
+
+	new Product({
+		imagePath: 'https://upload.wikimedia.org/wikipedia/en/5/5e/Gothiccover.png',
+		title:   'Gothic Video Game',
+		description: 'Awesome Game!!!!',
+		price: 50
+	}),
+
+	new Product({
+		imagePath: 'https://upload.wikimedia.org/wikipedia/en/5/5e/Gothiccover.png',
+		title:   'Gothic Video Game',
+		description: 'Awesome Game!!!!',
+		price: 60
+	})
+
+];
+
+var done = 0;
+for (var i = 0; i < products.length; i++){
+	products[i].save(function(err, result){
+		done++;
+		if(done == products.length){
+			exit();
+		}
+	});
+}
+
+function exit(){
+	mongoose.disconnect();
+}
